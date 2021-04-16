3f06846e3a16fd70a6e7768b0eb3fb33ff4ecb86
Austin Simon <austin@aust.nyc>
2021-04-16 17:10:54 -0400

    hide while load

--- components/VideoContainer.tsx
+++ components/VideoContainer.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 // Geist
@@ -10,9 +11,13 @@ type videoProps = {
 
 export default function VideoContainer(props: videoProps) {
   const [, setToast] = useToasts()
+  const [loaded, setLoaded] = useState(false)
+  function captureLoad() {
+    setLoaded(true)
+  }
   return (
-    <Card shadow style={{borderRadius: 20, cursor: 'pointer'}} onClick={() => setToast({text: 'hi!'})}>
-      <CameraImg src={props.url}/>
+    <Card shadow hidden={!loaded} style={{borderRadius: 20, cursor: 'pointer'}} onClick={() => setToast({text: 'coming soon'})}>
+      <CameraImg src={props.url} onLoad={captureLoad}/>
     </Card>
   )
 }
